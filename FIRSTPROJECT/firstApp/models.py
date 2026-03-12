46837612a2a36662405d02c04a45ba4b4ceb9505
abigailvalery76-ctrl <abigailvalery76@gmail.com>
2026-03-12 16:40:41 +0300

    there have been some crucial changes

--- FIRSTPROJECT/firstApp/models.py
+++ FIRSTPROJECT/firstApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    dob = models.DateField()
+    age = models.IntegerField()
+    course = models.CharField(max_length=100)
+    height = models.FloatField()
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    
+class hobby(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    requiremets = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
